e3fa26bcfb966fdf1933f59ddab7de58bbb688fa
cesnak <72663980+cesnak@users.noreply.github.com>
2021-04-22 12:18:59 +0200

    ZADANIE
    Napíš metódu, ktorá na vstupe bude prijímať desatinné číslo a na výstupe bude vracať toto číslo zaokrúhlené nahor. Vo vnútri použi metódu z triedy Math, ktorú si staticky importneš a potom ju budeš používať bez toho aby si zavolal Math.nazvoMetody() ale len nazovMetody().

---
+++ untitled20/src/important/Main.java
@@ -0,0 +1,20 @@
+package important;
+import static java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(rounding(2.8));
+
+    }
+
+
+    public static double rounding(double d){
+        return ceil(d);
+
+
+    }
+
+
+
+    }
+
